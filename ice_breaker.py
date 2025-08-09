0678aec608823911ed71265bd793c982ecce1443
mayankt28 <mayankt28@gmail.com>
2025-08-09 03:47:37 -0400

    output parser implemented

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,10 +1,11 @@
+from typing import Tuple
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from third_party.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from output_parser import summary_parser, Summary
 
-
-def ice_break_with(name: str) -> str:
+def ice_break_with(name: str) -> Tuple[Summary, str]:
     linkedinURL = linkedin_lookup_agent(name)
     linkedinData = scrape_linkedin_profile(linkedin_profile_url=linkedinURL)
 
@@ -18,14 +19,16 @@ def ice_break_with(name: str) -> str:
     summary_prompt_template = PromptTemplate(
         input_variables=['information'],
         template=summary_template,
+        partial_variables={"format_instructions": summary_parser.get_format_instructions()}
+        
         )
     llm = ChatOpenAI(
         model='gpt-4o-mini',
         temperature=0
     )
-    chain = summary_prompt_template | llm
-    res = chain.invoke({"information":linkedinData})
-    return res
+    chain = summary_prompt_template | llm | summary_parser
+    res: Summary = chain.invoke({"information":linkedinData})
+    return res, linkedinData.get('photoUrl')
 
 if __name__ == "__main__":
     print("Entering Icebreaker")
